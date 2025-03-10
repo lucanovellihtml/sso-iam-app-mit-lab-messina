1c937cbd33721660da3179bcd0a51ce57415c637
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-03-10 16:30:18 +0100

    Aggiornato: aggiunto il bean per la configurazione custom del FreeMarker

--- src/main/java/com/dxc/mit/iam/SsoIamAppMitApplication.java
+++ src/main/java/com/dxc/mit/iam/SsoIamAppMitApplication.java
@@ -2,12 +2,27 @@ package com.dxc.mit.iam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+import com.dxc.mit.iam.configuration.FreemarkerClassPathTldsLoader;
 
 @SpringBootApplication
 public class SsoIamAppMitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsoIamAppMitApplication.class, args);
+	}
+
+	/*
+	 * questo @Bean è responsabile di caricare,solo se non esiste già
+	 * l'implementazione, i Tag Library Descriptors per il motore di template
+	 * Freemarker
+	 */
+	@Bean
+	@ConditionalOnMissingBean({ FreemarkerClassPathTldsLoader.class })
+	public FreemarkerClassPathTldsLoader classPathTldsLoader() {
+		return new FreemarkerClassPathTldsLoader(new String[0]);
 	}
 
 }
