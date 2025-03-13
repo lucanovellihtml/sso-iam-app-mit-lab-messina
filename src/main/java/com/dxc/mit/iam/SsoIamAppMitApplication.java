69febb3db0268ee1f179de717fdad72b7b20f3c7
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-03-13 09:53:07 +0100

    Aggiunto: aggiunta l'esclusione della sorgente dati e la configurazione del metodo per lo start dell'app sul tomcat

--- src/main/java/com/dxc/mit/iam/SsoIamAppMitApplication.java
+++ src/main/java/com/dxc/mit/iam/SsoIamAppMitApplication.java
@@ -3,12 +3,28 @@ package com.dxc.mit.iam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import com.dxc.mit.iam.configuration.FreemarkerClassPathTldsLoader;
 
-@SpringBootApplication
-public class SsoIamAppMitApplication {
+/**
+ * il tag "exclude = {DataSourceAutoConfiguration.class}" serve per escludere la
+ * configurazione automatica della sorgente dati.
+ * l'estensione alla classe SpringBootServletInitializer per definire il metodo "configure" come punto di accesso dell'applicazione per il server su cui è deployata(Tomcat).
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SsoIamAppMitApplication extends SpringBootServletInitializer {
+
+	/**
+	 * Questo metodo specifica il main dell'applicazione(al server Tomcat) come punto di partenza per lo start sul tomcat.
+	 */
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SsoIamAppMitApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsoIamAppMitApplication.class, args);
