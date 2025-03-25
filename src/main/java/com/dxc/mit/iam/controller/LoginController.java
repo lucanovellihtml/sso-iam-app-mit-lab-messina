9b74306c15f0daf83f43c7cf1fe430e884b5d43e
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-03-25 17:21:06 +0100

    Aggiornato: aggiunto il mapping che gestisce la logout post utenza già acceduta, la logout post utente che non accetta la privacy d'iscrizione,la error not-found e la logout apacs (da controllare il suo funzionamento)

--- src/main/java/com/dxc/mit/iam/controller/LoginController.java
+++ src/main/java/com/dxc/mit/iam/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.dxc.mit.iam.controller;
 
 import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,13 +12,17 @@ import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.util.WebUtils;
 
+import com.dxc.mit.iam.model.IdPUserUpdate;
 import com.dxc.mit.iam.util.URLUtil;
 import com.fasterxml.jackson.databind.JsonSerializable.Base;
 
@@ -27,7 +34,13 @@ import jakarta.servlet.http.HttpServletResponse;
 // @RequestMapping("/custom-login")
 public class LoginController extends BaseController {
 
+    private final BaseController baseController;
+
     private static final Logger log = LoggerFactory.getLogger(com.dxc.mit.iam.controller.LoginController.class);
+
+    LoginController(BaseController baseController) {
+        this.baseController = baseController;
+    }
 
     /*
      * Controller pagina home con i pulsanti spid/spid aziende/cie.
@@ -128,6 +141,140 @@ public class LoginController extends BaseController {
             modelAndView.addObject("idpListUrl", this.config.getIdpListUrl());
             modelAndView.setViewName("/sso/login");
         }
+        return modelAndView;
+    }
+
+    /**
+     * Controller pagina post logout richiamata quando un'utenza, già acceduta,
+     * vuole disconnetersi.
+     * 
+     * @return La pagina di logout
+     */
+    @RequestMapping(value = { "/postlogout" }, method = { RequestMethod.GET })
+    public ModelAndView logout() {
+        log.debug("Request logout page");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/sso/logout");
+        return modelAndView;
+    }
+
+    /**
+     * Controller pagina logout richiamata quando non viene accettata la privacy
+     * durante la creazione dell'utenza spid.
+     * 
+     * @param headers Mappa che contiene gli header HTTP della richiesta
+     * @param request Oggetto che rappresenta la richiesta HTTP ricevuta
+     * @param goTo    Parametro della query string opzionale, specifica la URL di
+     *                destinazione dopo il logout; il nome del parametro viene preso
+     *                dalla variabile nel file yml.
+     * @return La pagina di logout
+     */
+    @RequestMapping(value = { "/postlogin/logout" }, method = { RequestMethod.GET })
+    public ModelAndView redirectToLogout(@RequestHeader Map<String, String> headers, HttpServletRequest request,
+            @RequestParam(required = false, name = "${sso.targetUrl.param}") String goTo) {
+        log.debug("entrato in metodo logout per setting isImpersonate a NO");
+        ModelAndView modelAndView = new ModelAndView();
+
+        // Oggetto redirect per reindirizzare l'utente all'endpoint /logout
+        RedirectView redirect = new RedirectView("/logout");
+
+        /*
+         * Creazione dell'oggetto model IdPUserUpdate per poi richiamare i metodi
+         * per settare employeeNumber e impersonate nuovi.
+         * Questo oggetto poi verra utilizzato come sostituto all'oggetto stesso
+         * presente,ma con i parametri vecchi, nel metodo di aggiornamento
+         * updateIdpUser.
+         */
+        IdPUserUpdate idPUser = new IdPUserUpdate();
+        idPUser.setEmployeeNumber(Collections.emptyList());
+        idPUser.setIsImpersonate("NO");
+        try {
+            updateIdpUser(idPUser, request, headers);
+            log.debug("employeeNumber resettato correttamente ed isImpersonate settato a NO");
+        } catch (Exception e) {
+            log.error("ERRORE IN METODO redirectToLogout");
+            log.error(e.getMessage());
+        }
+
+        /*
+         * Indica che la redirezione è relativa al contesto dell'applicazione(true).
+         * ES: Se il contesto dell'app è /customlogin, e la redirezione è su /home,
+         * l'utente sarà reindirizzato a: http://localhost:8080/customlogin/home
+         */
+        redirect.setContextRelative(true);
+
+        /*
+         * Permette di esporre le variabili di percorso nella URL di redirezione.
+         * ES: Se l'URL di redirezione contiene qualcosa come /utente/{id}, allora {id}
+         * sarà disponibile come variabile nel modello; allora nel modello potremmo
+         * accedere a {id} con il suo valore.
+         */
+        redirect.setExposePathVariables(true);
+
+        // Permette di mantenere gli attributi dell'oggetto model dopo la redirezione
+        redirect.setExposeModelAttributes(true);
+
+        modelAndView.setView((View) redirect);
+
+        /*
+         * Se la goTo è null o vuota usa un valore di default preso da
+         * this.config.getUndoLogoutRedirectUrl() che è il valore preso dalla variabile
+         * del file yml di configurazione ---> "${sso.undoLogoutRedirectUrl}".
+         * Altrimenti verrà mantenuto il valore originale della goTo passata come
+         * parametro nella richiesta GET.
+         * Controlla il formato della goTo attraverso la regola recuperata dalla
+         * variabile del file yml di configurazione ---> "${sso.targetUrl.pattern}".
+         * Se è valido, il goTo viene aggiunto come attributo al model.
+         */
+        goTo = Optional.<String>ofNullable(goTo).orElse(this.config.getUndoLogoutRedirectUrl());
+        if (URLUtil.isValidTargetUrl(goTo, this.config.getTargetUrlPattern()))
+            modelAndView.addObject("goto", goTo);
+        return modelAndView;
+    }
+
+    /**
+     * TODO da verificare che sia utilizzata
+     * 
+     * @param headers Mappa che contiene gli header HTTP della richiesta
+     * @param request Oggetto che rappresenta la richiesta HTTP ricevuta
+     * @param goTo    Parametro della query string opzionale, specifica la URL di
+     *                destinazione dopo il logout; il nome del parametro viene preso
+     *                dalla variabile nel file yml.
+     * @return La pagina di logout
+     */
+    @RequestMapping(value = { "/apacs-logout" }, method = { RequestMethod.GET })
+    public ModelAndView apacsLogout(@RequestHeader Map<String, String> headers, HttpServletRequest request,
+            @RequestParam(required = false, name = "${sso.targetUrl.param}") String goTo) {
+        ModelAndView modelAndView = new ModelAndView();
+        RedirectView redirect = new RedirectView("/logout");
+        IdPUserUpdate idPUser = new IdPUserUpdate();
+        idPUser.setEmployeeNumber(Collections.emptyList());
+        idPUser.setIsImpersonate("NO");
+        try {
+            updateIdpUser(idPUser, request, headers);
+            log.debug("employeeNumber resettato correttamente ed isImpersonate settato a NO");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        redirect.setContextRelative(true);
+        redirect.setExposePathVariables(true);
+        redirect.setExposeModelAttributes(true);
+        modelAndView.setView((View) redirect);
+        goTo = Optional.<String>ofNullable(goTo).orElse(this.config.getLogoutRedirectUrl());
+        if (URLUtil.isValidTargetUrl(goTo, this.config.getTargetUrlPattern()))
+            modelAndView.addObject("goto", goTo);
+        return modelAndView;
+    }
+
+    /**
+     * Controller che richiama la pagina di errore not-found.
+     * 
+     * @return la pagina di errore not-found
+     */
+    @RequestMapping(value = { "/not-found" }, method = { RequestMethod.GET })
+    public ModelAndView notFound() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/error/not-found");
         return modelAndView;
     }
 
