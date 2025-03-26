1b948098024600eec929f412e4cf9102e1c38179
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-03-26 10:39:29 +0100

    Aggiornato: aggiunto il metodo che gestisce la fase di postLogin e il metodo che restituisce la stringa nome e cognome formattata dell'utente che ha acceduto(getNomeECognomeUtenteConnesso)

--- src/main/java/com/dxc/mit/iam/controller/LoginController.java
+++ src/main/java/com/dxc/mit/iam/controller/LoginController.java
@@ -1,14 +1,18 @@
 package com.dxc.mit.iam.controller;
 
 import java.security.Principal;
+import java.text.ParseException;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,10 +25,17 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.util.WebUtils;
-
+import com.dxc.mit.iam.configuration.FreemarkerClassPathTldsLoader;
+import com.dxc.mit.iam.model.IdPUserResponse;
 import com.dxc.mit.iam.model.IdPUserUpdate;
+import com.dxc.mit.iam.model.pcsi.utente.PrgIps;
+import com.dxc.mit.iam.model.pcsi.utente.PrgPosAna;
+import com.dxc.mit.iam.model.pcsi.utente.Tcpi1ipf;
+import com.dxc.mit.iam.model.pcsi.utente.UtentePln;
 import com.dxc.mit.iam.util.URLUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializable.Base;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -34,13 +45,10 @@ import jakarta.servlet.http.HttpServletResponse;
 // @RequestMapping("/custom-login")
 public class LoginController extends BaseController {
 
-    private final BaseController baseController;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private static final Logger log = LoggerFactory.getLogger(com.dxc.mit.iam.controller.LoginController.class);
-
-    LoginController(BaseController baseController) {
-        this.baseController = baseController;
-    }
 
     /*
      * Controller pagina home con i pulsanti spid/spid aziende/cie.
@@ -141,6 +149,221 @@ public class LoginController extends BaseController {
             modelAndView.addObject("idpListUrl", this.config.getIdpListUrl());
             modelAndView.setViewName("/sso/login");
         }
+        return modelAndView;
+    }
+
+    /**
+     * Controller che gestisce la logica della fase di post-login.
+     * 
+     * @param headers  Mappa contenente gli header HTTP inviati dal client
+     * @param request  Oggetto che rappresenta la richiesta HTTP
+     * @param response Oggetto che rappresenta la risposta HTTP
+     * @return La pagina in base alla situazione di login dell'utente
+     * @throws ParseException
+     * @throws JsonProcessingException
+     */
+    @RequestMapping(value = { "/postlogin" }, method = { RequestMethod.GET })
+    public ModelAndView postLogin(@RequestHeader Map<String, String> headers, HttpServletRequest request,
+            HttpServletResponse response) throws ParseException, JsonProcessingException {
+        ModelAndView modelAndView = new ModelAndView();
+
+        log.debug("post login method");
+
+        // Recupera la goTo dalla sessione l'URL di destinazione dopo il login (se
+        // esiste)
+        String goTo = (String) request.getSession().getAttribute(this.config.getTargetUrlParam());
+        log.debug("Found goTo parameter: " + goTo);
+
+        /*
+         * Controlla se nell'header è presente l'attributo "isfirstaccess" e se è
+         * valorizzato, altrimenti di default attributisce il valore a "yes".
+         */
+        String isFirstAccessHeader = Optional.<String>ofNullable(headers.get(this.config.getHeaderParamNamePostLogin()))
+                .orElse("yes");
+        log.debug("Found MIMSpostLogin parameter: " + isFirstAccessHeader);
+
+        /*
+         * Recupera dati utente dagli header HTTP:
+         * 
+         * idUtente: Identificativo dell'utente.
+         * minAge: Età minima richiesta per accedere.
+         * nascitaData: Data di nascita dell'utente.
+         * dateFormat: Formato della data di nascita.
+         */
+        String idUtente = headers.get(this.config.getHeaderParamNameUserId());
+        int minAge = this.config.getMinAge();
+        log.debug("Found minAge parameter: " + minAge);
+        String nascitaData = headers.get(this.config.getHeaderParamNameDataNascita());
+        log.debug("Found nascitaData parameter: " + nascitaData);
+        String dateFormat = this.config.getDataNascitaFormat();
+        log.debug("Found dateFormat parameter: " + minAge);
+        log.debug("UTENTE-------------------: " + idUtente);
+
+        /*
+         * Controlla se l'ID utente è nullo o vuoto e in caso reindirizza alla pagina di
+         * errore.
+         * La pagina di errore è settata nella variabile del file yml --->
+         * "${sso.pdtRedirectUrl}"
+         */
+        if (!StringUtils.hasText(idUtente)) {
+            modelAndView.setView((View) redirect(this.config.getPdtRedirectUrl(), false));
+            return modelAndView;
+        }
+
+        /*
+         * Controlla se l'utente è maggiorenne e in caso reindirizza alla pagina di
+         * errore.
+         * La pagina di errore è settata nella variabile del file yml --->
+         * "${sso.pdtRedirectUrl}"
+         */
+        if (checkAge(nascitaData, dateFormat, minAge)) {
+            log.debug("age <18");
+            modelAndView.setView((View) redirect(this.config.getPdtRedirectUrl(), false));
+            return modelAndView;
+        }
+
+        /*
+         * Vengono recuperati i dati dell'utente attraverso il metodo getIdpUser.
+         * Il metodo getIdpUser accetta come parametro la request dell'IDP e l'headers
+         * della request, da cui elabora ed estrare i dati per costruire l'oggetto
+         * idpUser.
+         * In caso di fallimento viene restituita la pagina di errore
+         * "/postlogin/errorLogout".
+         */
+        IdPUserResponse idpUser = new IdPUserResponse();
+        log.debug("Recupero utente su IDP");
+        try {
+            log.error("header passata ad utente = " + headers.toString());
+            idpUser = getIdpUser(request, headers);
+        } catch (Exception e) {
+            log.error("Errore su recupero utente = " + e.getCause());
+            return getErrorModelAndView(modelAndView, "postlogin.generic.error.message",
+                    this.config.getRedirectTimeout(), null, "/postlogin/errorLogout");
+        }
+        log.debug("Utente recuperato correttamente");
+
+        /*
+         * Controlla se l'utente sta già impersonando un altro account.
+         * In caso affermativo viene restituita la pagina di errore
+         * "/postlogin/errorLogout".
+         */
+        Boolean isImpersonate = Boolean.valueOf(
+                ((List) Optional.<List>ofNullable(idpUser.getIsImpersonate()).orElse(Collections.emptyList())).stream()
+                        .anyMatch(y -> ((String) y).equalsIgnoreCase("YES")));
+        log.debug("Utente ha gia' sessione di impersonate attiva {}", isImpersonate);
+        if (Boolean.TRUE.equals(isImpersonate))
+            return getErrorModelAndView(modelAndView, "postlogin.select.profile.error.already-impersonating",
+                    this.config
+                            .getRedirectTimeout(),
+                    this.config.getPdtRedirectUrl(), "/postlogin/errorLogout");
+        log.debug("Recupero Matricole attive per l'utente e abilitate all'accesso SPID");
+
+        /*
+         * Vengono recuperate le matricole attive per l'utente che ha acceduto.
+         * Le matricole vengono recuperate utilizzando il CODICE FISCALE dell'utente che
+         * ha acceduto.
+         * Se non ci sono matricole attive, viene resettato l'employeeNumber e
+         * reindirizzato l'utente alla "/postlogin/logout".
+         * In caso di errore viene restituita la pagina di errore
+         * "/postlogin/errorLogout".
+         */
+        List<UtentePln> elencoMatricoleUtentePln = Collections.emptyList();
+        try {
+            UtentePln utente = new UtentePln();
+            utente.setPrgNodoAdsp(new Integer(this.config.getNodoAdsp()));
+            utente.setDesNomUte("");
+            utente.setTcpi1ipf(new Tcpi1ipf());
+            utente.getTcpi1ipf().setPrgPosAna(new PrgPosAna());
+            utente.getTcpi1ipf().getPrgPosAna().setCodFisPerFsc(headers.getOrDefault(this.config.getHeaderName(), ""));
+            utente.getTcpi1ipf().getPrgPosAna().setDesNomPerFis("");
+            utente.getTcpi1ipf().getPrgPosAna().setDesCogPerFis("");
+            utente.getTcpi1ipf().setPrgIps(new PrgIps());
+            utente.getTcpi1ipf().getPrgIps().setDesDenIps("");
+            utente.getTcpi1ipf().getPrgIps().setCodParIvaIps("");
+            elencoMatricoleUtentePln = this.userService.findUtentiByCodiceFiscale(utente);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return getErrorModelAndView(modelAndView, "postlogin.generic.error.message",
+                    this.config.getRedirectTimeout(), null, "/postlogin/errorLogout");
+        }
+        log.debug("Matricole attive per l'utente e abilitate all'accesso SPID recuperate: {}",
+                Integer.valueOf(elencoMatricoleUtentePln.size()));
+        if (CollectionUtils.isEmpty(elencoMatricoleUtentePln)) {
+            log.debug("Nessuna matricola trovata, resetto per sicurezza l'attributo employeenumber");
+            IdPUserUpdate idPUser = new IdPUserUpdate();
+            idPUser.setEmployeeNumber(Collections.emptyList());
+            try {
+                updateIdpUser(idPUser, request, headers);
+                log.debug("employeeNumber resettato correttamente");
+                modelAndView.addObject("error_msg", "postlogin.generic.error.message");
+                modelAndView.setView((View) redirect("/postlogin/logout"));
+            } catch (Exception e) {
+                log.error(e.getMessage());
+                return getErrorModelAndView(modelAndView, "postlogin.generic.error.message",
+                        this.config.getRedirectTimeout(), null, "/postlogin/errorLogout");
+            }
+            modelAndView.addObject("nomeEcognome", getNomeEcognomeUtenteConnesso(headers));
+            return modelAndView;
+        }
+        request.getSession().setAttribute("elencoMatricoleUtente", elencoMatricoleUtentePln);
+
+        /*
+         * Controllo l'accettazione della privacy.
+         * Se è il primo accesso e quindi isFirstAccessHeader è "yes", allora mostra la
+         * pagina di privacy.
+         * Altrimenti completa la login(Utente già acceduto in precedenza).
+         */
+        log.debug("Utente ha gia' accettato la privacy {}", isFirstAccessHeader);
+        if ("yes".equalsIgnoreCase(isFirstAccessHeader)) {
+            modelAndView.addObject("nomeEcognome", getNomeEcognomeUtenteConnesso(headers));
+            modelAndView.setViewName("/postlogin/privacy");
+            modelAndView.addObject("logoutTimeout", Integer.valueOf(300000));
+        } else {
+            modelAndView = acceptPrivacy(headers, request, response);
+        }
+        return modelAndView;
+    }
+
+    /**
+     * Controller che gestisce la fase di post accept privacy dell'utente; viene
+     * chiamato dopo il login dell'utente quando accetta l'informativa sulla
+     * privacy.
+     * Dopo l'accettazione, l'utente viene reindirizzato alla selezione del profilo
+     * 
+     * @param headers  Mappa che contiene gli header HTTP della richiesta
+     * @param request  Oggetto che rappresenta la richiesta HTTP
+     * @param response Oggetto che rappresenta la risposta HTTP
+     * @return la pagina di selezione dei profili post login
+     * @throws JsonProcessingException
+     */
+    @RequestMapping(value = { "/postlogin/accept-privacy" }, method = { RequestMethod.POST })
+    public ModelAndView acceptPrivacy(@RequestHeader Map<String, String> headers, HttpServletRequest request,
+            HttpServletResponse response) throws JsonProcessingException {
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        // Memorizzazione nella sessione dell'utente l'attributo privacy a true
+        request.getSession().setAttribute("privacy", Boolean.TRUE);
+
+        /*
+         * Recupero delle matricole associate all'utente che ha acceduto.
+         * Cerca nella sessione la lista delle matricole e controlla se sono presenti,
+         * altrimenti restituisce una lista vuota.
+         */
+        List<UtentePln> elencoMatricoleUtente = Optional
+                .<List<UtentePln>>ofNullable(
+                        (List<UtentePln>) request.getSession().getAttribute("elencoMatricoleUtente"))
+                .orElse(Collections.emptyList());
+
+        /*
+         * Costruzione dei dati del modello da passare nella view.
+         * L'utente ha un time massimo di 3 minuti d'inattività prima di essere
+         * disconnesso dalla sessione.
+         */
+        modelAndView.addObject("nomeEcognome", getNomeEcognomeUtenteConnesso(headers));
+        modelAndView.addObject("elencoMatricoleUtente", this.objectMapper.writeValueAsString(elencoMatricoleUtente));
+        modelAndView.setViewName("/postlogin/select-profile");
+        modelAndView.addObject("logoutTimeout", Integer.valueOf(180000));
         return modelAndView;
     }
 
@@ -276,6 +499,25 @@ public class LoginController extends BaseController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("/error/not-found");
         return modelAndView;
+    }
+
+    /**
+     * Metodo che recupera il nome e cognome dell'utente dai valori degli header
+     * HTTP e li formatta in una stringa.
+     * 
+     * @param headers Mappa che contiene gli header HTTP ricevuti nella richiesta
+     * @return la stringa nome e cognome dell'utente connesso
+     */
+    private String getNomeEcognomeUtenteConnesso(Map<String, String> headers) {
+        /*
+         * Recupero dall'header,attraverso l'attrivbuto apacs-name e apacs-surname, il
+         * nome e cognome dell'utente
+         * connession, altrimenti se non sono presenti restituisce una stringa vuota.
+         * La stringa viene formattata e restituita.
+         */
+        return String.format("%s %s",
+                new Object[] { headers.getOrDefault("apacs-name", ""), headers.getOrDefault("apacs-surname", "") })
+                .trim();
     }
 
 }
