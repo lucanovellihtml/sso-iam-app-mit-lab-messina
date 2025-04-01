bf28127c061b198526091bd25c2c7c4610958a71
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-04-01 15:33:30 +0200

    Aggiunto: aggiunta la classe che rappresenta un'identificativo di un'entità(comune o provincia).

---
+++ src/main/java/com/dxc/mit/iam/model/pcsi/utente/Id.java
@@ -0,0 +1,63 @@
+package com.dxc.mit.iam.model.pcsi.utente;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/*
+ * Classe che rappresenta un'identificativo di un'entità(comune o provincia)
+ */
+public class Id {
+  /*
+   * Lista attributi con i relativi metodi set e get.
+   * 
+   * prgIps: Numero progressivo di identificazione
+   * 
+   * prgMebIps: Numero progressivo di identificazione per membri
+   * 
+   * codCom: Codice identificativo del comune
+   * 
+   * codPro: Codice identificativo della provincia
+   */
+  int prgIps;
+
+  int prgMebIps;
+
+  String codCom;
+
+  String codPro;
+
+  @JsonProperty("prgIps")
+  public int getPrgIps() {
+    return this.prgIps;
+  }
+
+  public void setPrgIps(int prgIps) {
+    this.prgIps = prgIps;
+  }
+
+  @JsonProperty("prgMebIps")
+  public int getPrgMebIps() {
+    return this.prgMebIps;
+  }
+
+  public void setPrgMebIps(int prgMebIps) {
+    this.prgMebIps = prgMebIps;
+  }
+
+  @JsonProperty("codCom")
+  public String getCodCom() {
+    return this.codCom;
+  }
+
+  public void setCodCom(String codCom) {
+    this.codCom = codCom;
+  }
+
+  @JsonProperty("codPro")
+  public String getCodPro() {
+    return this.codPro;
+  }
+
+  public void setCodPro(String codPro) {
+    this.codPro = codPro;
+  }
+}
